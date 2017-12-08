2af04e9a078ccbcce7947af9db0f712b01ba09fe
Armando Ruiz <armando.ruiz@industrie.co>
2017-12-08 17:00:02 +1100

    Activated route for dynamic show page

---
+++ app/proposal/proposal-show.component.ts
@@ -0,0 +1,25 @@
+import { Component, Oninit } from '@angular/core'; //oninit to load things as soon as the app loads
+import { ActivatedRoute, Params } from '@angular/router'; // the ability to see the route for the proposal and the id
+import { Proposal } from './proposal'
+
+@Component({
+	moduleId: module.id,
+	selector: 'proposal-show',
+	templateUrl: 'proposal-show.component.html'
+})
+export class ProposalShowComponent implements OnInit {
+  id: number;
+  routeId: any;
+  
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+  
+  ngOnInit(): void {
+    this.routeId = this.route.params.subscribe(
+      params => {
+        this.id = +params['id']; //the + converts the string into a number
+      }
+    )
+  }
+}
