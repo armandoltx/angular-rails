4ef4c539e83ea724d172da954bfebbc0d721412d
Armando Ruiz <armando.ruiz@industrie.co>
2017-12-11 09:53:37 +1100

    fixing bug

--- app/proposal/proposal-show.component.ts
+++ app/proposal/proposal-show.component.ts
@@ -1,6 +1,6 @@
-import { Component, Oninit } from '@angular/core'; //oninit to load things as soon as the app loads
-import { ActivatedRoute, Params } from '@angular/router'; // the ability to see the route for the proposal and the id
-import { Proposal } from './proposal'
+import { Component, OnInit } from '@angular/core';//oninit to load things as soon as the app loads
+import { ActivatedRoute, Params } from '@angular/router';// the ability to see the route for the proposal and the id
+import { Proposal } from './proposal';
 
 @Component({
 	moduleId: module.id,
@@ -8,18 +8,18 @@ import { Proposal } from './proposal'
 	templateUrl: 'proposal-show.component.html'
 })
 export class ProposalShowComponent implements OnInit {
-  id: number;
-  routeId: any;
-  
-  constructor(
-    private route: ActivatedRoute
-  ) {}
-  
-  ngOnInit(): void {
-    this.routeId = this.route.params.subscribe(
-      params => {
-        this.id = +params['id']; //the + converts the string into a number
-      }
-    )
-  }
+	id: number;
+	routeId: any;
+
+	constructor(
+		private route: ActivatedRoute
+	) {}
+
+	ngOnInit(): void {
+		this.routeId = this.route.params.subscribe(
+				params => {
+					this.id = +params['id'];//the + converts the string into a number
+				}
+			)
+	}
 }
